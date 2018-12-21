9762530bb62c3e88dea9269447a1dbc1711edd25
Vitaliy Pavlyuk <vitaliy.pavlyuk@team.neustar>
2018-12-20 22:14:43 -0500

    modified to reproduce https://github.com/spring-cloud/spring-cloud-gateway/issues/728

--- src/main/java/com/example/demogateway/DemogatewayApplication.java
+++ src/main/java/com/example/demogateway/DemogatewayApplication.java
@@ -2,58 +2,55 @@ package com.example.demogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 @RestController
 @SpringBootApplication
 public class DemogatewayApplication {
 
-	@RequestMapping("/hystrixfallback")
-	public String hystrixfallback() {
-		return "This is a fallback";
+	@Bean
+	public CorsConfiguration corsConfiguration(RoutePredicateHandlerMapping routePredicateHandlerMapping) {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList(CorsConfiguration.ALL)));
+		corsConfiguration.setAllowedMethods(Arrays.asList(
+				HttpMethod.POST.name(),
+				HttpMethod.GET.name(),
+				HttpMethod.OPTIONS.name(),
+				HttpMethod.DELETE.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.PATCH.name()
+		));
+		corsConfiguration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList(CorsConfiguration.ALL)));
+		corsConfiguration.setMaxAge(3600L);
+		corsConfiguration.setAllowCredentials(true);
+		routePredicateHandlerMapping.setCorsConfigurations(
+				new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
+		return corsConfiguration;
 	}
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		//@formatter:off
 		return builder.routes()
-				.route("path_route", r -> r.path("/get")
-						.uri("http://httpbin.org"))
-				.route("host_route", r -> r.host("*.myhost.org")
-						.uri("http://httpbin.org"))
-				.route("rewrite_route", r -> r.host("*.rewrite.org")
-						.filters(f -> f.rewritePath("/foo/(?<segment>.*)",
-								"/${segment}"))
-						.uri("http://httpbin.org"))
-				.route("hystrix_route", r -> r.host("*.hystrix.org")
-						.filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-								.uri("http://httpbin.org"))
-				.route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
-						.filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-								.uri("http://httpbin.org"))
-				.route("limit_route", r -> r
-					.host("*.limited.org").and().path("/anything/**")
-						.filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
-					.uri("http://httpbin.org"))
-				.route("websocket_route", r -> r.path("/echo")
-					.uri("ws://localhost:9000"))
+				.route("path_backend", r -> r.path("/v2/version")
+						.uri("http://localhost:38492/v2/version"))
 				.build();
 		//@formatter:on
-	}
-
-	@Bean
-	RedisRateLimiter redisRateLimiter() {
-		return new RedisRateLimiter(1, 2);
 	}
 
 	@Bean
